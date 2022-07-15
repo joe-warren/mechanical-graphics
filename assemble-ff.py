1d43665979d405a101388a446c38304a340bf589
Joseph Warren <hungryjoe.warren@gmail.com>
2022-07-15 12:17:03 +0100

    Update script to support fonts with different weights

--- assemble-ff.py
+++ assemble-ff.py
@@ -2,15 +2,19 @@
 import fontforge 
 import string
 import xml.etree.ElementTree as ET
+import os
 from os.path import exists
 from math import ceil
+import re
 ns = {"svg": "http://www.w3.org/2000/svg"}
 
-def filename(fontname, char):
-    return f'{fontname}/{char}.svg'
-
-def when_glyph_defined(font, fontname, char):
-    f = filename(fontname, char)
+def filename(fontname, char, weight=None):
+    if weight == None:
+       return f'{fontname}/{char}.svg'
+    else:
+       return f'{fontname}/{char}-{weight}.svg'
+def when_glyph_defined(font, fontname, char, weight):
+    f = filename(fontname, char, weight)
     charSvg = ET.parse(f)
     svg =charSvg.getroot()
     height = float(svg.attrib['height'])
@@ -34,24 +38,47 @@ def when_glyph_defined(font, fontname, char):
     font.descent = ceil(height * 10)
 
     print(f"{fontname} - {char} : {width} x {height}")
-    print(glyph.boundingBox())
 
-def build_font(fontname):
+def get_weights(fontname):
+  files = os.listdir(fontname) 
+  r = re.compile("^.-([A-z]*)\.svg$")
+  
+  variants = set(r.match(f).group(1) for f in files if r.match(f)) 
+  variants.add("Regular")
+  return variants
+
+def build_weighted_font(fontname, weight=None):
     font=fontforge.font()    
     chars = string.ascii_letters + string.digits + "&"
     
     for char in chars:
-       if exists(filename(fontname, char)): 
-           when_glyph_defined(font, fontname, char)
+       if exists(filename(fontname, char, weight)): 
+           when_glyph_defined(font, fontname, char, weight)
+       elif exists(filename(fontname, char)): 
+           when_glyph_defined(font, fontname, char, None)
     dashed=fontname.replace(" ", "-").replace(".", "")
     font.familyname=f"Mechanical-Graphics-{dashed}"
     font.fontname=fontname.replace(" ", "_")
     font.fullname=f"Mechanical Graphics {fontname}"
+  
     font.copyright="This font is in the public domain"
-    font.save(f"output/{dashed}.sfd")  
-    font.generate(f"output/{dashed}.ttf")
+    name = dashed
+    if weight == None:
+        font.weight = "Regular"
+    else:
+        font.weight = weight
+        name = f"{dashed}-{weight}"
+    font.save(f"output/{name}.sfd")  
+    font.generate(f"output/{name}.ttf")
     print(f"finished {fontname}")
 
-fonts = ["No. 1", "No. 2", "No. 3"]
+def build_font(fontname):
+    weights = get_weights(fontname)
+    if(len(weights) == 1):
+      build_weighted_font(fontname)
+    else:
+      for weight in get_weights(fontname):
+        build_weighted_font(fontname, weight)
+fonts = ["No. 1", "No. 2", "No. 3", "No. 4"]
 for f in fonts:
     build_font(f)
